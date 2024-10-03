b3239bb57c966afb70dd12b4a384795f619709af
DIANA-ATIENO-source <atienowitney2002@gmail.com>
2024-10-03 15:58:20 +0300

    Update Loan_qualification[1].c

--- Loan_qualification[1].c
+++ Loan_qualification[1].c
@@ -1,8 +1,8 @@
 //program to implement whether a client qualifies for a loan
 /*
-Authot:Diana
+Author:Diana
 Date:28/09/2024
-CT101/G/23407/24
+REG NO:CT101/G/23407/24
 */
 #include<stdio.h>
 int main(){
@@ -20,10 +20,6 @@ int main(){
 
    else{"Unfortunately,we are unable to offer you a loan at at this time";}
    
-
-
-
-
 return 0;
 }
 
